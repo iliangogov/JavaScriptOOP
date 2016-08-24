55dd0bbd765c32f172bedefc7c733467b75bdf46
iliangogov <iliangogov@gmail.com>
2016-08-24 17:37:02 +0300

    functions task-2.js solved

--- Topics/01. Functions-and-Function-Expressions/homework/tasks/task-2.js
+++ Topics/01. Functions-and-Function-Expressions/homework/tasks/task-2.js
@@ -6,8 +6,45 @@
 		3) it must throw an Error if any of the range params is missing
 */
 
-function findPrimes() {
-	
+function findPrimes(from, to) {
+	var primes = [];
+
+	if (typeof(from) === 'undefined' || typeof(to) === 'undefined') {
+		throw 'Error';
+	}
+
+	var from = +from;
+	var to = +to;
+
+	if (isNaN(from) || isNaN(to)) {
+		throw 'Error';
+	}
+
+	function isPrime(num) {
+
+		if (num < 2) {
+			return false;
+		}
+		
+			var prime = true;
+
+		for (var i = 2; i <= Math.sqrt(num); i += 1) {
+			if (num % i == 0) {
+				prime = false;
+			}
+		}
+
+		return prime;
+	}
+
+	for (var j = from; j <= to; j += 1) {
+
+		if (isPrime(j)) {
+			primes.push(j);
+		}
+	}
+
+	return primes;
 }
 
 module.exports = findPrimes;
