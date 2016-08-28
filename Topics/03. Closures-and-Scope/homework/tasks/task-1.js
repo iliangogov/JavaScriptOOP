de0b09af60f4330c6f640b83c7a010034a0bdfec
iliangogov <iliangogov@gmail.com>
2016-08-28 12:57:22 +0300

    ClosuresAndScopeHW added

--- Topics/03. Closures-and-Scope/homework/tasks/task-1.js
+++ Topics/03. Closures-and-Scope/homework/tasks/task-1.js
@@ -20,22 +20,86 @@
 			*	Book ISBN is an unique code that contains either 10 or 13 digits
 			*	If something is not valid - throw Error
 */
+
 function solve() {
 	var library = (function () {
 		var books = [];
 		var categories = [];
+
 		function listBooks() {
-			return books;
+			if (!arguments.length) {
+				return books;
+			}
+			var prop = arguments[0].category,
+				booksWithProp = [],
+				i,
+				len = books.length;
+
+			for (i = 0; i < len; i += 1) {
+				if (books[i].category === prop) {
+					booksWithProp.push(books[i]);
+				}
+			}
+
+			return booksWithProp;
 		}
 
 		function addBook(book) {
-			book.ID = books.length + 1;
+			if ((book.title.length < 2 || book.title.length > 100) ||
+				!(book.isbn.length == 13 || book.isbn.length == 10) || !book.author) {
+				throw new Error('No book with the provided title, author or isbn exists!');
+			}
+			if (!isUnique(book.title, 'title') || !isUnique(book.isbn, 'isbn')) {
+				throw new Error('Book of the same title, author or ISBN exists already!');
+			}
+
+			if (!book.category) {
+				book.category = 'Uncategorized';
+			}
+
+			var categoryDawg = {
+				category: book.category,
+				ID: categories.length + 2
+			};
+
+			book.ID = books.length + 2;
+
+			if(categories.length === 0){
+				categories.push(categoryDawg);
+			}
+			else if(categories && !categories.some(function(elem){
+					return elem.category === categoryDawg.category;
+				})){
+				categories.push(categoryDawg);
+			}
+
 			books.push(book);
 			return book;
 		}
 
+
 		function listCategories() {
+			categories = categories.sort(function(a, b) {
+				return a.ID - b.ID;
+			});
+			categories = categories.map(function(item){
+				return item.category;
+			});
 			return categories;
+		}
+
+		function isUnique(bookProp, prop) {
+			var i,
+				len = books.length;
+			if (len == 0) {
+				return true;
+			}
+			for (i = 0; i < len; i += 1) {
+				if (books[i][prop] == bookProp) {
+					return false;
+				}
+			}
+			return true;
 		}
 
 		return {
